654f49c66954ae3e3a4eed03090c856a087a9af7
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-12-08 13:04:24 +0100

    feat: Create More Articles

---
+++ scripts/get-all-articles.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Fetch articles from articles.html and append to the articles-container div
+    fetch('all-articles.html')
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('.articles-container').innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching articles:', error));
+});
