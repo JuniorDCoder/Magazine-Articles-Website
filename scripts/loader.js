43011923bf9969b5c666652df301543c8f6a70b9
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-12-08 11:14:00 +0100

    feat: Design Header Navigation

---
+++ scripts/loader.js
@@ -0,0 +1,10 @@
+// loader.js
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.loader-container');
+    const mainContent = document.querySelector('.main-content');
+
+    setTimeout(function () {
+        loader.style.display = 'none';
+        mainContent.style.display = 'block';
+    }, 5000);
+});
