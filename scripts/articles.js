a64db1ed2e84bac834c07a83e976a35f8a045fd2
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-12-14 05:10:23 +0100

    more fixes

--- scripts/articles.js
+++ scripts/articles.js
@@ -6,7 +6,8 @@ let articles = [
         description: "Description of the first article content goes here.",
         authorImage: "public/images/profile.png",
         authorName: "Nyuydine Bill",
-        publishDate: "Thursday, 2nd October 2004"
+        publishDate: "Thursday, 2nd October 2004",
+        category: "Nature",
     },
     {
         id: 2,
@@ -15,7 +16,8 @@ let articles = [
         description: "Description of the second article content goes here.",
         authorImage: "public/images/profile.png",
         authorName: "Foryoung Junior",
-        publishDate: "Thursday, 30th September 2004"
+        publishDate: "Thursday, 30th September 2004",
+        category: "Wildlife",
     },
     {
         id: 3,
@@ -24,7 +26,8 @@ let articles = [
         description: "Description of the first article content goes here.",
         authorImage: "public/images/profile.png",
         authorName: "Nyuydine Bill",
-        publishDate: "Publication Date 1"
+        publishDate: "Publication Date 1",
+        category: "Technology",
     },
     {
         id: 4,
@@ -33,9 +36,49 @@ let articles = [
         description: "Description of the second article content goes here.",
         authorImage: "public/images/profile.png",
         authorName: "Foryoung Junior",
-        publishDate: "Thursday, 30th September 2004"
+        publishDate: "Thursday, 30th September 2004",
+        category: "Fashion",
     },
-    
+    {
+        id: 5,
+        image: "public/images/articles/5.jpg",
+        title: "Exploring the Rainforest",
+        description: "Discover the wonders hidden within the rainforest.",
+        authorImage: "public/images/profile.png",
+        authorName: "Elena Rivers",
+        publishDate: "Wednesday, 15th November 2023",
+        category: "Travel",
+      },
+      {
+        id: 6,
+        image: "public/images/articles/6.jpg",
+        title: "The Impact of Renewable Energy",
+        description: "Examining the role of renewables in our future.",
+        authorImage: "public/images/profile.png",
+        authorName: "Alex Green",
+        publishDate: "Saturday, 5th December 2023",
+        category: "Science",
+      },
+      {
+        id: 7,
+        image: "public/images/articles/7.jpg",
+        title: "Diving into Marine Biology",
+        description: "Unraveling mysteries beneath the ocean's surface.",
+        authorImage: "public/images/profile.png",
+        authorName: "Marina Fisher",
+        publishDate: "Monday, 28th November 2023",
+        category: "Biology",
+      },
+      {
+        id: 8,
+        image: "public/images/articles/8.png",
+        title: "Elegance in Simplicity: Minimalist Fashion",
+        description: "Embracing simplicity and style in the fashion world.",
+        authorImage: "public/images/profile.png",
+        authorName: "Sophie Ward",
+        publishDate: "Friday, 2nd December 2023",
+        category: "Fashion",
+    },    
 ];
 
 
@@ -55,6 +98,7 @@ function generateArticleHTML(article, includeButtons = false) {
                     <p class="publish-date">${article.publishDate}</p>
                 </div>
             </div>
+            <p class="article-category">${article.category}</p>
             ${includeButtons ? `<button class="like-button" onclick="like(event)">&#128077;</button>
             <button class="comment-button" onclick="comment(event)">Comment</button>` : ''}
         </div>
